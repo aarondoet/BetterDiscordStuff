aa2302d36e176daca8d784770da3182217e422d2
Strencher <46447572+Strencher@users.noreply.github.com>
2020-09-23 19:36:56 +0200

    -.-'

--- Plugins/TypingIndicator/TypingIndicator.plugin.js
+++ Plugins/TypingIndicator/TypingIndicator.plugin.js
@@ -14,7 +14,7 @@ var TypingIndicator = (() => {
 			name: "TypingIndicator",
 			authors: [{name: "l0c4lh057", github_username: "l0c4lh057", twitter_username: "l0c4lh057", discord_id: "226677096091484160"}],
 			description: "Shows an indicator in the guild/channel list when someone is typing there",
-			version: "0.4.0",
+			version: "0.4.1",
 			github: "https://github.com/l0c4lh057/BetterDiscordStuff/blob/master/Plugins/TypingIndicator/",
 			github_raw: "https://raw.githubusercontent.com/l0c4lh057/BetterDiscordStuff/master/Plugins/TypingIndicator/TypingIndicator.plugin.js"
 		},
